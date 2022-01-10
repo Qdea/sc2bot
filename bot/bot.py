c6586b8d09de5581f713a877504bedb5fd9b0dcb
Qdea <ivanovanatoliy1506@gmail.com>
2022-01-10 14:07:15 +0300

    my changes

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,10 @@
-from sc2 import BotAI, Race
+from sc2 import Race, BotAI
 
 
 class CompetitiveBot(BotAI):
     NAME: str = "CompetitiveBot"
     """This bot's name"""
-    RACE: Race = Race.Terran
+    RACE: Race = Race.Protoss
     """This bot's Starcraft 2 race.
     Options are:
         Race.Terran
